1fddd25f464ba60a40cc0d1c23a8a42f9eb60c6c
ArcaneAj <41121914+ArcaneAj@users.noreply.github.com>
2025-05-23 16:25:41 +1200

    Finished website

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ namespace Root {
     document
         .getElementById('createButton')!
         .addEventListener('click', () => generateProblems(100), false);
+    document.addEventListener('DOMContentLoaded', () => generateProblems(100));
 
     enum Operation {
         Add = 1,
@@ -42,25 +43,71 @@ namespace Root {
                 case 3:
                     return `${this.a} x ${this.b} =`;
                 default:
-                    console.log(this.operation);
-                    console.log(typeof this.operation);
                     throw new Error('Invalid Operation');
             }
+        }
+
+        resultString(): string {
+            return this.result().toString();
+        }
+
+        generateHTML(): HTMLElement {
+            const root = document.createElement('div') as HTMLDivElement;
+            root.classList.add('item');
+
+            const question = document.createElement('div') as HTMLDivElement;
+            question.classList.add('question');
+            question.innerText = this.format();
+
+            const answer = document.createElement('div') as HTMLDivElement;
+            answer.classList.add('answer');
+            answer.style.visibility = 'hidden';
+
+            const input = document.createElement('input') as HTMLInputElement;
+            input.classList.add('input');
+            input.setAttribute('type', 'number');
+            input.setAttribute('step', '1');
+            input.addEventListener('blur', () => {
+                const ans = input.value;
+
+                if (ans === this.resultString()) {
+                    input.style.backgroundColor = 'green';
+                    answer.innerText = '✓';
+                    answer.style.visibility = 'visible';
+                    answer.style.color = 'green';
+                    input.disabled = true;
+                    const scoreElem = document.getElementById(
+                        'score'
+                    ) as HTMLDivElement;
+                    const oldScore = parseInt(
+                        scoreElem.innerText.split('/')[0]
+                    );
+                    scoreElem.innerText = `${oldScore + 1}/100`;
+                }
+                if (ans !== this.resultString() && ans !== '') {
+                    input.style.backgroundColor = 'red';
+                    answer.innerText = this.resultString();
+                    answer.style.visibility = 'visible';
+                    answer.style.color = 'red';
+                    input.disabled = true;
+                }
+            });
+
+            root.appendChild(question);
+            root.appendChild(input);
+            root.appendChild(answer);
+
+            return root;
         }
     }
 
     function generateProblems(n: number) {
         const problems = Array.from({ length: n }, (_, i) => new Problem());
-        console.log(problems.map((x) => x.format()));
-        console.log(problems.map((x) => x.result()));
-    }
-
-    function getRandomEnumValue<T extends { [s: string]: any }>(
-        enumObject: T
-    ): T[keyof T] {
-        const enumValues = Object.values(enumObject) as T[keyof T][];
-        const randomIndex = Math.floor(Math.random() * enumValues.length);
-        return enumValues[randomIndex];
+        const grid = document.getElementById('grid') as HTMLDivElement;
+        grid.replaceChildren();
+        for (const problem of problems) {
+            grid.appendChild(problem.generateHTML());
+        }
     }
 
     function randomIntFromInterval(min: number, max: number) {
